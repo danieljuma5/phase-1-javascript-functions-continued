1bc5e04ef758b8843fadd0ee6787261e03f81da5
danieljuma5 <jumadaniel275@gmail.com>
2023-03-15 03:28:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// function declaration
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(sugar = "*") {
+  return function(adj = "special") {
+    return `You are ${sugar}${adj}${sugar}!`
+  }
+
+}
+wrapAdjective("%")("a dedicated programmer");
